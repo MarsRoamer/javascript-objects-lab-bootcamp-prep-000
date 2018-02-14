fa85614f6a6d128a1dad90501c63cdf052b93a78
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-14 22:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return obj[key] = value;
+  var newObj = Objects.assign({}. obj);
+  newObj[key] = value;
+  return newObj;
 
 }
